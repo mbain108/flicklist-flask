525c4879e862c1d059da11190876913341527611
Melissa Bain <apps@melissadbain.com>
2018-09-13 18:45:04 -0500

    Added code for distinct movie for tomorrow

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +9,10 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
+    tomorrows_movie = get_random_movie()
+
+    while tomorrows_movie == movie:
+        tomorrows_movie = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
@@ -17,13 +22,20 @@ def index():
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    content += "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + tomorrows_movie + "</li>"
+    content += "</ul>"
 
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
-    # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    movie_titles = ["The Incredibles", "Toy Story", "The Robinsons", "Get Smart", "Charlie's Angles"];
 
+    # TODO: randomly choose one of the movies, and return it
+    position = random.randrange(0, len(movie_titles) - 1);
+
+    return movie_titles[position]
 
 app.run()
