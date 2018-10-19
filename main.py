163d53eccda99283f36a0c9a60693ce2c23b96d9
Melissa Bain <apps@melissadbain.com>
2018-10-19 16:12:46 -0500

    Studio 8 Completed

--- main.py
+++ main.py
@@ -1,38 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
-from flask_sqlalchemy import SQLAlchemy
 import cgi
-
-app = Flask(__name__)
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:MyNewPass@localhost:8889/flicklist'
-app.config['SQLALCHEMY_ECHO'] = True
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
-
-    def __repr__(self):
-        return '<User %r>' % self.email
-
-class Movie(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    watched = db.Column(db.Boolean)
-    rating = db.Column(db.String(20))
-
-    def __init__(self, name):
-        self.name = name
-        self.watched = False
-
-    def __repr__(self):
-        return '<Movie %r>' % self.name
+from models import User, Movie
+from app import app, db
 
 # a list of movie names that nobody should have to watch
 terrible_movies = [
@@ -44,10 +13,12 @@ terrible_movies = [
 ]
 
 def get_current_watchlist():
-    return Movie.query.filter_by(watched=False).all()
+    owner = User.query.filter_by(email=session['user']).first()
+    return Movie.query.filter_by(watched=False, owner=owner).all()
 
 def get_watched_movies():
-    return Movie.query.filter_by(watched=True).all()
+    owner = User.query.filter_by(email=session['user']).first()
+    return Movie.query.filter_by(watched=True, owner=owner).all()
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -164,7 +135,9 @@ def add_movie():
         error = "Trust me, you don't want to add '{0}' to your Watchlist".format(new_movie_name)
         return redirect("/?error=" + error)
 
-    movie = Movie(new_movie_name)
+    owner = User.query.filter_by(email=session['user']).first()
+
+    movie = Movie(new_movie_name, owner)
     db.session.add(movie)
     db.session.commit()
     return render_template('add-confirmation.html', movie=movie)
